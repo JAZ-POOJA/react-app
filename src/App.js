e7756d64b19a1eb15bf9130c8c9b9fc19ee0a324
prakash <prakashjaiswal625@gmail.com>
2022-06-26 01:19:53 +0530

    deploy my react app to github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import Axios from "axios";
+
+import CityComponent from "./modules/CityComponent";
+import WeatherComponent from "./modules/WeatherInfoComponent";
+
+export const WeatherIcons = {
+  "01d": "/icons/tree.svg",
+  "01n": "/icons/cloud-icon.svg",
+  "02d": "/icons/sunrise.svg",
+  "02n": "/icons/moon.svg",
+  "03d": "/icons/verycloudy.svg",
+  "03n": "/icons/cloud.svg",
+  "04d": "/icons/tree.svg",
+  "05d": "/icons/smile-cloud.svg",
+  "50n": "/icons/smile-cloud.svg",
+  "04n": "/icons/night.svg",
+  "09d": "/icons/rain.svg",
+  "09n": "/icons/mountain.svg",
+  "10d": "/icons/raindrops.svg",
+  "10n": "/icons/raincloud.svg",
+  "11d": "/icons/smile-cloud.svg",
+  "11n": "/icons/storm.svg",
+};
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 380px;
+  padding: 20px 10px 10px 20px;
+  margin: auto;
+  border-radius: 4px;
+  box-shadow: 0 3px 6px 0 #555;
+  background: white;
+  font-family: Montserrat;
+`;
+
+const AppLabel = styled.span`
+  color: black;
+  margin: 20px auto;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
 
 function App() {
+  const[city,updateCity] = useState();
+  const[weather,updateWeather] = useState();
+  const fetchWeather = async(e) => {
+    e.preventDefault();
+    const response = await Axios.get(
+         ` https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=389ab5f855b1635a19596ecdf07f2e0d`,
+    );
+    updateWeather(response.data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+
+      <AppLabel><h2> Weather App </h2> </AppLabel>
+       {city && weather ? (
+        <WeatherComponent weather = {weather } city = {city} />
+       ): (
+        <CityComponent updateCity = { updateCity} fetchWeather = { fetchWeather} />
+       ) }
+      
+      
+     
+    </Container>
   );
 }
 
